2a7e4527903305428ba87bdb641e5ff6bf57f8d1
artemklimenko96 <Artemklimenko96@gmail.com>
2016-10-26 03:09:36 +0300

    main logic files uploaded

---
+++ index.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Navbar Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="./css/navbar.css" rel="stylesheet">
+
+  </head>
+
+
+     <?php
+if(!empty($_GET["loginerror"])) $errormessage='<div class="alert alert-danger" role="alert">Error in login. Retype username and password</div>';else $errormessage='';
+?>
+<form class="form-signin" method="post" action="login.php">
+    <h2 class="form-signin-heading">Please login</h2>
+        <label for="username" class="sr-only">Username</label>
+        <input type="text" id="username" name="username" class="form-control" placeholder="username" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+    <?php if(!$errormessage=='') echo $errormessage;?>
+</form>
+    
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="./js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+
+function clearstring($string){
+	//removes unwanted characters from any variable when called
+	$string=stripslashes($string);  //unquotes the string
+	$string=htmlentities($string);  //html-tags
+	$string=strip_tags($string);    //html/php tags
+	
+	return $string;
+}
+include('db.php');
+//use database connection
+
+ if(isset($_POST['username'])) $username=clearstring($_POST['username']);
+ else $username='';
+ if(isset($_POST['password'])) $password=clearstring($_POST['password']);
+ else $password='';
+ //query the database table user to check if username+password
+ //are correct
+ $sql="select username from user where username='$username' and ";
+ $sql.="password='$password' ";
+ $result=$conn->query($sql);  //runs the SQL query, returns $result
+ if ($result->num_rows > 0){
+	 //username and password were correct
+	 $row=$result->fetch_assoc();
+	 session_start();
+	 $_SESSION['user']=$row['username'];
+	 header("location:./admin/index.php");
+ }else header("location:index.php?page=login&loginerror=true");  //username and password were not correct
+
+$conn->close();
+?>
